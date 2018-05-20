9e18c829e9e67d20d86865ee4b23ea07d7f7fdfa
escolmebartlebooth <escolme@bartlebooth.co.uk>
2018-05-20 20:42:57 +0100

    debug delta t

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -130,6 +130,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
   float dt_3 = dt_2 * dt;
   float dt_4 = dt_3 * dt;
 
+  cout << dt << endl;
   //Modify the F matrix so that the time is integrated
   //ekf_.F_(0, 2) = dt;
   //ekf_.F_(1, 3) = dt;
