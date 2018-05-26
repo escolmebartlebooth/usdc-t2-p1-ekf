3f745ade001b013800151d97aa400cf5549d886b
escolmebartlebooth <escolme@bartlebooth.co.uk>
2018-05-26 15:27:45 +0100

    debug l or r only

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -145,6 +145,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
          dt_3/2*noise_ax_, 0, dt_2*noise_ax_, 0,
          0, dt_3/2*noise_ay_, 0, dt_2*noise_ay_;
 
+  cout << "predict..." << endl;
   ekf_.Predict();
 
   /*****************************************************************************
@@ -160,12 +161,14 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR
      && measurement_usage !=2) {
     // Radar updates
+    cout << "update RADAR..." << endl;
     Hj_ = tools.CalculateJacobian(ekf_.x_);
     ekf_.H_ = Hj_;
     ekf_.R_ = R_radar_;
     ekf_.UpdateEKF(measurement_pack.raw_measurements_);
   } else if (measurement_usage !=1) {
     // Laser updates
+    cout << "update LIDAR..." << endl;
     ekf_.H_ = H_laser_;
     ekf_.R_ = R_laser_;
     ekf_.Update(measurement_pack.raw_measurements_);
